0373037fa9020135560fa41d8d3c17ee4c67a9bb
javaphil <philip@spare5.com>
2018-09-21 15:23:24 -0700

    inital server commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-// change
+import express from 'express'
+import bodyParser from 'body-parser'
+import logger from 'morgan'
+import mongoose from 'mongoose'
+
+const app = express()
+const router = express.Router()
+
+const API_PORT = process.env.API_PORT || 3001
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(logger('dev'))
+
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello World' })
+})
+
+app.use('/api', router)
+
+app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`))
