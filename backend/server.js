8132dde4e02a3d83274a57a875c93387827a04f4
Mollie Harms <mollie.harms@gmail.com>
2018-11-01 20:51:23 -0700

    got the db seed in if there are no documents in the resources collection, added material ui library

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import logger from 'morgan'
 import mongoose from 'mongoose'
+import dbSeed from '../seeds/resources.json'
 
 import Resource from './models/resource'
+import { isNullOrUndefined } from 'util';
 
 const app = express()
 const router = express.Router()
@@ -21,12 +23,53 @@ db.once('open', function (callback) {
   console.log('connection successful')
 })
 
+
+db.collection('resources').countDocuments(function(err, count) {
+  console.dir(err);
+  console.dir(count);
+
+  if( count == 0) {
+      console.log("No Found Records.");
+
+      //const seeds = dbSeed;
+      for (var seed of dbSeed){
+        console.log(seed, 'seed')
+        const resource = new Resource(seed);
+        resource.save();
+       /* const { title, mediaSrc, description } = seed.body;
+        if (!title || !mediaSrc || !description) {
+          return res.json({
+            success: false,
+            error: 'You must provide a title, mediaSrc source, and description'
+          })
+        }
+
+        resource.title = title;
+        resource.mediaSrc = mediaSrc;
+        resource.description = description;
+        resource.save(err => {
+          if (err) return res.json({ success: false, error: err })
+          return res.json({ success: true })
+        })*/
+      }
+      //db.collection('resources').data = seed;
+  }
+  else {
+      console.log("Found Records : " + count);
+  }
+})
+
+
 router.get('/', (req, res) => {
   res.json({ message: 'Hello World' })
+  //res.json({ message: 'test' })s
+
+  
 })
 
 router.get('/resources', (req, res) => {
   let db = req.db
+
   Resource.find((err, resources) => {
     if (err) return res.json({ success: false, error: err })
     return res.json({ success: true, data: resources })
@@ -35,6 +78,7 @@ router.get('/resources', (req, res) => {
 
 router.post('/resources', (req, res) => {
   let db = req.db
+  
   const resource = new Resource()
   const { title, mediaSrc, description } = req.body
   if (!title || !mediaSrc || !description) {
@@ -52,6 +96,7 @@ router.post('/resources', (req, res) => {
     return res.json({ success: true })
   })
 })
+
 
 app.use('/api', router)
 
--- client/src/components/ResourceList.js
+++ client/src/components/ResourceList.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import './ResourceList.css'
 import ResourceListItem from './ResourceListItem'
-
+/*
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import GridListTileBar from '@material-ui/core/GridListTileBar'
+import IconButton from '@material-ui/core/IconButton'
+import StarBorderIcon from '@material-ui/icons/StarBorder'
+import tileData from './tileData'
+*/
 const ResourceList = (props) => {
   const resourceItems = props.data.map(resource => (
     <ResourceListItem title={resource.title}
